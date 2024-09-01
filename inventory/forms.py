d82b7400a253b8c43a5e3650cdfae42d39d0c44e
unknown <QingMarcel>
2024-09-01 02:27:24 +0100

    First commit

---
+++ inventory/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Category, InventoryItem
+
+
+class UserRegisterForm(UserCreationForm):
+  email = forms.EmailField()
+
+  class meta:
+    model = User
+    fields = ['username', 'email' 'password1', 'password2']
+
+
+class InventoryItemForm(forms.ModelForm):
+  category = forms.ModelChoiceField(queryset=Category.objects.all(), initial=0)
+  class Meta:
+    model = InventoryItem
+    fields = ['name', 'quantity', 'category']
